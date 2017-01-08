a406254f8fbd742e0e5df20733c198d16abf90a2
Andrew Ringler <ringler@gmail.com>
2017-01-08 18:16:10 -0500

    picking up cubes

--- roll-ball-tutorial/Assets/Scripts/PlayerControl.cs
+++ roll-ball-tutorial/Assets/Scripts/PlayerControl.cs
@@ -21,5 +21,11 @@ public class PlayerControl : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * speed);
 	}
-		
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+		}
+	}
 }
